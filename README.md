63314844f0be9a0526980da74ad569d7ee095a10
Linless- <linless56@gmail.com>
2016-08-21 13:33:53 +0500

    change README

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
 #  Перенос музыки из ВК в ГуглМузыку
+
+### Необходимые действия
+1. Установиться nodejs версии [5] (https://nodejs.org/dist/latest-v5.x/) или выше
+2. Получить access_token на сайте VK
+3. Что бы было подключено мобильное устройство к гугл аккаунту
+4. Отключить двухэтапная аутентификация в Google аккаунте :(
+
+### Получение access_token
+1. Переходим по ссылке и разрешаем действия http://oauth.vk.com/authorize?client_id=5598052&scope=audio&%20redirect_uri=https://oauth.vk.com/blank.html&display=page&v=5.0&response_type=token
+2. Вас перекинет на другую страницу
+3. Скопировать токен из адресной строки
+
+### Установка
+1. npm i
+2. node index.js --token ACCESS_TOKEN --user_id USER_ID --email GOOGLE_EMAIL --password GOOGLE_PASSWORD --name_list NAME_PLAY_LIST
+
+### Описание флагов
+* USER_ID - айдишник пользователя, у которого берём музыку (не обязательно свой). Айдишник можно взять если перейти на кладку "музыка" в ВК. Он будет в дресной строке. Пример: https://vk.com/audios19811123 -> нам нужны только цифры
+* GOOGLE_EMAIL - почта, под которой нужно авторизоваться
+* GOOGLE_PASSWORD - пароль для авторизации
+* NAME_PLAY_LIST - название плейлиста, куда нужно закачать музыку
--- index.js
+++ index.js
@@ -8,56 +8,82 @@ const argv = require('minimist')(process.argv.slice(2));
 const progress = require('progress');
 const colors = require('colors');
 
-let notTracks = '';
-let notArtist = '';
-let countSuccess = 0;
-let bar;
-
 let token = argv.token;
 let user_id = argv.user_id;
 let email = argv.email;
 let password = argv.password;
 let playListName = argv.name_list;
-// console.log(argv.token);
-// d7f9f14b2bf7a69ff1682f704571a8ba6813163b48425dcb907a119d39a223ee10a962f4f319dbe01a394
-// 19811600
 
-if (!token) {
-    throw Error('Не указан токен от VK.')
+if (!token) throw Error('Не указан токен от VK.')
+if (!user_id) throw Error('Не указан user_id от VK.')
+if (!email) throw Error('Не указана почта от google.')
+if (!password) throw Error('Не указан пароль от google.')
+if (!playListName) throw Error('Не указано имя плейлиста.')
+
+let successResult = () => {
+    console.log(colors.magenta('========================'));
+    console.log(colors.magenta('     Процесс завершен   '));
+    console.log(colors.magenta('========================'));
+    console.log('\n');
+    console.log(colors.magenta('Удачно перенеслось: ' + countSuccess + '/' + countTracks));
+    console.log('\n');
+    console.log(colors.magenta('------- Список песен, которые не нашлись, но есть исполнитель -------'));
+    console.log(notTracks);
+    console.log('\n');
+    console.log(colors.magenta('------- Список песен, которые не нашлись и нет исполнителя -------'));
+    console.log(notArtist);
 }
 
-if (!user_id) {
-    throw Error('Не указан user_id от VK.')
+let addTrack = (numb) => {
+    bar.tick();
+    currentTrack++;
+    if (numb >= countTracks || undefined == numb) {
+        successResult();
+        return;
+    }
+
+    pm.search(result[numb], 1, (err, data) => {
+        if (data.entries) {
+            for (let i = 0; i < data.entries.length; i++) {
+                if (data.entries[i].hasOwnProperty('track')) {
+                    let trackId = data.entries[i].track.nid;
+                    pm.addTrackToPlayList(trackId, playListId, (err, data) => {
+                        if (err) console.log(err);
+                        countSuccess++;
+                        addTrack(currentTrack);
+                    });
+                    return;
+                }
+            }
+
+            notTracks += result[numb] + '\n';
+            addTrack(currentTrack);
+        } else {
+            notArtist += result[numb] + '\n';
+            addTrack(currentTrack);
+        }
+    });
 }
 
-if (!email) {
-    throw Error('Не указана почта от google.')
-}
-
-if (!password) {
-    throw Error('Не указан пароль от google.')
-}
-
-if (!playListName) {
-    throw Error('Не указано имя плейлиста.')
-}
-
+let body = [], result = [], notTracks = '', notArtist = '', countSuccess = 0, bar;
 let options = 'https://api.vk.com/method/audio.get?count=0&owner_id=' + user_id + '&access_token=' + token;
-let body = [], result = [];
 https.get(options, (request) => {
     request
-        .on('data', function(chunk) {
-            body.push(chunk);
+        .on('data', (chunk) => {
+            body.push(chunk)
         })
-        .on('end', function() {
-            body = JSON.parse(Buffer.concat(body).toString()).response;
-
+        .on('end', () => {
+            body = JSON.parse(Buffer.concat(body).toString());
+            if (!body.response) {
+                console.log('VK Error: ' + body.response);
+                return;
+            }
+            body = body.response;
             for (let i = 1; i < body.length; i++) {
                 result.push(body[i].artist + ' - ' + body[i].title);
             }
 
-            let countTracks = result.length;
-            let currentTrack = 0;
+            let countTracks = result.length, currentTrack = 0;
             let barOptions = {
                 total: countTracks,
                 width: 35
@@ -66,54 +92,13 @@ https.get(options, (request) => {
 
             pm.init({email: email, password: password}, (err) => {
                 if (err) {
-                    console.log(err);
+                    console.log('Google Error: ' + err);
                     return;
                 }
                 let playListId;
                 pm.addPlayList(playListName, (err, body) => {
                     playListId = body.mutate_response[0].id;
-                    let addTrack = (numb) => {
-                        bar.tick();
-                        currentTrack++;
-                        if (numb >= countTracks || undefined == numb) {
-                            console.log(colors.magenta('========================'));
-                            console.log(colors.magenta('     Процесс завершен   '));
-                            console.log(colors.magenta('========================'));
-                            console.log('\n');
-                            console.log(colors.magenta('Удачно перенеслось: ' + countSuccess + '/' + countTracks));
-                            console.log('\n');
-                            console.log(colors.magenta('------- Список песен, которые не нашлись, но есть исполнитель -------'));
-                            console.log(notTracks);
-                            console.log('\n');
-                            console.log(colors.magenta('------- Список песен, которые не нашлись и нет исполнителя -------'));
-                            console.log(notArtist);
-                            return;
-                        }
-
-                        pm.search(result[numb], 1, (err, data) => {
-                            if (data.entries) {
-                                for (let i = 0; i < data.entries.length; i++) {
-                                    if (data.entries[i].hasOwnProperty('track')) {
-                                        let trackId = data.entries[i].track.nid;
-                                        pm.addTrackToPlayList(trackId, playListId, (err, data) => {
-                                            if (err) console.log(err);
-                                            countSuccess++;
-                                            addTrack(currentTrack);
-                                        });
-                                        return;
-                                    }
-                                }
-
-                                notTracks += result[numb] + '\n';
-                                addTrack(currentTrack);
-                            } else {
-                                notArtist += result[numb] + '\n';
-                                addTrack(currentTrack);
-                            }
-                        });
-                    }
                     addTrack(currentTrack);
-
                 });
             });
         });
